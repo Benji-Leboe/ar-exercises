363b5474c1e28a2cec5a187a6faf813437258a40
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-10-10 16:56:05 -0700

    finish ex.7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Bob", last_name: "Saget", hourly_rate: 100)
+@store1.employees.create(first_name: "Tomato", last_name: "Pancakes", hourly_rate: 100)
+@store1.employees.create(first_name: "Ted", last_name: "Bundy", hourly_rate: 100)
+
+@store2.employees.create(first_name: "Dr. Crocatron", last_name: "Bitches", hourly_rate: 100)
+@store2.employees.create(first_name: "Dixon", last_name: "Cider", hourly_rate: 100)
+@store2.employees.create(first_name: "Butt", last_name: "Lord", hourly_rate: 100)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+@store_name = gets.chomp
+
+@newstore = Store.create(name: @store_name)
+
+@newstore.errors.each { |attr, message| puts "#{attr}: #{message}" }
